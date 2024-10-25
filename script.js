37e811eb00472f16e7eb4f093cc8fe080cc3b91a
Jet <jetagatep@gmail.com>
2024-10-25 21:56:45 +0800

    Made the basic UI for the calculator without CSS and now displays numbers when buttons are clicked

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function calculate(num1, operator, num2) {
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
+function operate(num1, operator, num2) {
     let result = 0;
 
     switch(operator) {
@@ -16,3 +20,12 @@ function calculate(num1, operator, num2) {
             break;
     }
 }
+
+const equalsBtn = document.querySelector('#equals');
+
+document.addEventListener("click", function(evnt){
+    const display = document.querySelector('.displayNumber h1');
+    let previousNumber = evnt.target.id.split('-')[1];
+    display.textContent = previousNumber;
+});
+
